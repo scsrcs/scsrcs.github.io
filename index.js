fc824222ede12fa4f3a69e436e9b6abcfd6935e1
Arunav ganguly <63651649+weblaze@users.noreply.github.com>
2021-07-02 19:36:51 +0530

    updates

--- index.js
+++ index.js
@@ -56,4 +56,12 @@ $(document).ready(function(){
   autoplaySpeed: 3000,
   adaptiveHeight: true
 });
+  $('.eventcar').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000
+  });
 });
