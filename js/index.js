aecc7415cc4e459afa37d3514a1d26ec8da46894
SRCS Student Council <86761255+scsrcs@users.noreply.github.com>
2021-07-05 07:31:25 +0530

    EVENT GRID AND MODALS DONE
    the bootstrap iso thing did not work turns out it wasn't the library I needed so I deleted all of bootstrap and went back and looked through the bulma documentation and figured out a way to make a COMPLETELY RESPONSIVE grid and I added all of the popups for it (javascript done- appearance not done). Now that this is done I am finally going to have a half hour nap before waking up for class. Fuck you.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,429 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  
+  //Register Popup
   const registerButton = document.getElementById("Register");
-  const modalBg = document.getElementById("mdback");
-  const modalClose = document.getElementById("mdclose");
-  const modalCancel = document.getElementById("mdcancel");
-  const modal = document.getElementById("mds");
+  const Rmodal = document.getElementById("Rmd");
+  const RmodalBg = document.getElementById("Rmdbg");
+  const RmodalClose = document.getElementById("Rmdclose");
+  const RmodalCancel = document.getElementById("Rmdcancel");
 
   registerButton.addEventListener('click', () => 
   {
-    modal.classList.add("is-active");
+    Rmodal.classList.add("is-active");
   });
-  modalBg.addEventListener('click', () => 
+    RmodalBg.addEventListener('click', () => 
   {
-    modal.classList.remove("is-active");
+    Rmodal.classList.remove("is-active");
   });
-  modalCancel.addEventListener('click', () => 
+    RmodalCancel.addEventListener('click', () => 
   {
-    modal.classList.remove("is-active");
+    Rmodal.classList.remove("is-active");
   });
-  modalClose.addEventListener('click', () => 
+    RmodalClose.addEventListener('click', () => 
   {
-    modal.classList.remove("is-active");
+    Rmodal.classList.remove("is-active");
   });
+
+  //J1 Popup
+  const J1Button = document.getElementById("J1");
+  const J1modal = document.getElementById("J1md");
+  const J1modalBg = document.getElementById("J1bg");
+  const J1modalClose = document.getElementById("J1close");
+
+  J1Button.addEventListener('click', () => 
+  {
+    J1modal.classList.add("is-active");
+  });
+
+    J1modalBg.addEventListener('click', () => 
+  {
+    J1modal.classList.remove("is-active");
+  });
+
+    J1modalClose.addEventListener('click', () => 
+  {
+    J1modal.classList.remove("is-active");
+  });
+
+  //J2 Popup
+  const J2Button = document.getElementById("J2");
+  const J2modal = document.getElementById("J2md");
+  const J2modalBg = document.getElementById("J2bg");
+  const J2modalClose = document.getElementById("J2close");
+
+  J2Button.addEventListener('click', () => 
+  {
+    J2modal.classList.add("is-active");
+  });
+
+    J2modalBg.addEventListener('click', () => 
+  {
+    J2modal.classList.remove("is-active");
+  });
+
+    J2modalClose.addEventListener('click', () => 
+  {
+    J2modal.classList.remove("is-active");
+  });
+
+  //J3 Popup
+  const J3Button = document.getElementById("J3");
+  const J3modal = document.getElementById("J3md");
+  const J3modalBg = document.getElementById("J3bg");
+  const J3modalClose = document.getElementById("J3close");
+
+  J3Button.addEventListener('click', () => 
+  {
+    J3modal.classList.add("is-active");
+  });
+
+    J3modalBg.addEventListener('click', () => 
+  {
+    J3modal.classList.remove("is-active");
+  });
+
+    J3modalClose.addEventListener('click', () => 
+  {
+    J3modal.classList.remove("is-active");
+  });
+
+  //J4 Popup
+  const J4Button = document.getElementById("J4");
+  const J4modal = document.getElementById("J4md");
+  const J4modalBg = document.getElementById("J4bg");
+  const J4modalClose = document.getElementById("J4close");
+
+  J4Button.addEventListener('click', () => 
+  {
+    J4modal.classList.add("is-active");
+  });
+
+    J4modalBg.addEventListener('click', () => 
+  {
+    J4modal.classList.remove("is-active");
+  });
+
+    J4modalClose.addEventListener('click', () => 
+  {
+    J4modal.classList.remove("is-active");
+  });
+
+  //J5 Popup
+  const J5Button = document.getElementById("J5");
+  const J5modal = document.getElementById("J5md");
+  const J5modalBg = document.getElementById("J5bg");
+  const J5modalClose = document.getElementById("J5close");
+
+  J5Button.addEventListener('click', () => 
+  {
+    J5modal.classList.add("is-active");
+  });
+
+    J5modalBg.addEventListener('click', () => 
+  {
+    J5modal.classList.remove("is-active");
+  });
+
+    J5modalClose.addEventListener('click', () => 
+  {
+    J5modal.classList.remove("is-active");
+  });
+
+  //J6 Popup
+  const J6Button = document.getElementById("J6");
+  const J6modal = document.getElementById("J6md");
+  const J6modalBg = document.getElementById("J6bg");
+  const J6modalClose = document.getElementById("J6close");
+
+  J6Button.addEventListener('click', () => 
+  {
+    J6modal.classList.add("is-active");
+  });
+
+    J6modalBg.addEventListener('click', () => 
+  {
+    J6modal.classList.remove("is-active");
+  });
+
+    J6modalClose.addEventListener('click', () => 
+  {
+    J6modal.classList.remove("is-active");
+  });
+
+  //S1 Popup
+  const S1Button = document.getElementById("S1");
+  const S1modal = document.getElementById("S1md");
+  const S1modalBg = document.getElementById("S1bg");
+  const S1modalClose = document.getElementById("S1close");
+
+  S1Button.addEventListener('click', () => 
+  {
+    S1modal.classList.add("is-active");
+  });
+
+    S1modalBg.addEventListener('click', () => 
+  {
+    S1modal.classList.remove("is-active");
+  });
+
+    S1modalClose.addEventListener('click', () => 
+  {
+    S1modal.classList.remove("is-active");
+  });
+
+  //S2 Popup
+  const S2Button = document.getElementById("S2");
+  const S2modal = document.getElementById("S2md");
+  const S2modalBg = document.getElementById("S2bg");
+  const S2modalClose = document.getElementById("S2close");
+
+  S2Button.addEventListener('click', () => 
+  {
+    S2modal.classList.add("is-active");
+  });
+
+    S2modalBg.addEventListener('click', () => 
+  {
+    S2modal.classList.remove("is-active");
+  });
+
+    S2modalClose.addEventListener('click', () => 
+  {
+    S2modal.classList.remove("is-active");
+  });
+
+  //S3 Popup
+  const S3Button = document.getElementById("S3");
+  const S3modal = document.getElementById("S3md");
+  const S3modalBg = document.getElementById("S3bg");
+  const S3modalClose = document.getElementById("S3close");
+
+  S3Button.addEventListener('click', () => 
+  {
+    S3modal.classList.add("is-active");
+  });
+
+    S3modalBg.addEventListener('click', () => 
+  {
+    S3modal.classList.remove("is-active");
+  });
+
+    S3modalClose.addEventListener('click', () => 
+  {
+    S3modal.classList.remove("is-active");
+  });
+
+  //S4 Popup
+  const S4Button = document.getElementById("S4");
+  const S4modal = document.getElementById("S4md");
+  const S4modalBg = document.getElementById("S4bg");
+  const S4modalClose = document.getElementById("S4close");
+
+  S4Button.addEventListener('click', () => 
+  {
+    S4modal.classList.add("is-active");
+  });
+
+    S4modalBg.addEventListener('click', () => 
+  {
+    S4modal.classList.remove("is-active");
+  });
+
+    S4modalClose.addEventListener('click', () => 
+  {
+    S4modal.classList.remove("is-active");
+  });
+
+  //S5 Popup
+  const S5Button = document.getElementById("S5");
+  const S5modal = document.getElementById("S5md");
+  const S5modalBg = document.getElementById("S5bg");
+  const S5modalClose = document.getElementById("S5close");
+
+  S5Button.addEventListener('click', () => 
+  {
+    S5modal.classList.add("is-active");
+  });
+
+    S5modalBg.addEventListener('click', () => 
+  {
+    S5modal.classList.remove("is-active");
+  });
+
+    S5modalClose.addEventListener('click', () => 
+  {
+    S5modal.classList.remove("is-active");
+  });
+
+  //S6 Popup
+  const S6Button = document.getElementById("S6");
+  const S6modal = document.getElementById("S6md");
+  const S6modalBg = document.getElementById("S6bg");
+  const S6modalClose = document.getElementById("S6close");
+
+  S6Button.addEventListener('click', () => 
+  {
+    S6modal.classList.add("is-active");
+  });
+
+    S6modalBg.addEventListener('click', () => 
+  {
+    S6modal.classList.remove("is-active");
+  });
+
+    S6modalClose.addEventListener('click', () => 
+  {
+    S6modal.classList.remove("is-active");
+  });
+
+  //S7 Popup
+  const S7Button = document.getElementById("S7");
+  const S7modal = document.getElementById("S7md");
+  const S7modalBg = document.getElementById("S7bg");
+  const S7modalClose = document.getElementById("S7close");
+
+  S7Button.addEventListener('click', () => 
+  {
+    S7modal.classList.add("is-active");
+  });
+
+    S7modalBg.addEventListener('click', () => 
+  {
+    S7modal.classList.remove("is-active");
+  });
+
+    S7modalClose.addEventListener('click', () => 
+  {
+    S7modal.classList.remove("is-active");
+  });
+
+  //S8 Popup
+  const S8Button = document.getElementById("S8");
+  const S8modal = document.getElementById("S8md");
+  const S8modalBg = document.getElementById("S8bg");
+  const S8modalClose = document.getElementById("S8close");
+
+  S8Button.addEventListener('click', () => 
+  {
+    S8modal.classList.add("is-active");
+  });
+
+    S8modalBg.addEventListener('click', () => 
+  {
+    S8modal.classList.remove("is-active");
+  });
+
+    S8modalClose.addEventListener('click', () => 
+  {
+    S8modal.classList.remove("is-active");
+  });
+
+  //S9 Popup
+  const S9Button = document.getElementById("S9");
+  const S9modal = document.getElementById("S9md");
+  const S9modalBg = document.getElementById("S9bg");
+  const S9modalClose = document.getElementById("S9close");
+
+  S9Button.addEventListener('click', () => 
+  {
+    S9modal.classList.add("is-active");
+  });
+
+    S9modalBg.addEventListener('click', () => 
+  {
+    S9modal.classList.remove("is-active");
+  });
+
+    S9modalClose.addEventListener('click', () => 
+  {
+    S9modal.classList.remove("is-active");
+  });
+
+  //S10 Popup
+  const S10Button = document.getElementById("S10");
+  const S10modal = document.getElementById("S10md");
+  const S10modalBg = document.getElementById("S10bg");
+  const S10modalClose = document.getElementById("S10close");
+
+  S10Button.addEventListener('click', () => 
+  {
+    S10modal.classList.add("is-active");
+  });
+
+    S10modalBg.addEventListener('click', () => 
+  {
+    S10modal.classList.remove("is-active");
+  });
+
+    S10modalClose.addEventListener('click', () => 
+  {
+    S10modal.classList.remove("is-active");
+  });
+
+  //S11 Popup
+  const S11Button = document.getElementById("S11");
+  const S11modal = document.getElementById("S11md");
+  const S11modalBg = document.getElementById("S11bg");
+  const S11modalClose = document.getElementById("S11close");
+
+  S11Button.addEventListener('click', () => 
+  {
+    S11modal.classList.add("is-active");
+  });
+
+    S11modalBg.addEventListener('click', () => 
+  {
+    S11modal.classList.remove("is-active");
+  });
+
+    S11modalClose.addEventListener('click', () => 
+  {
+    S11modal.classList.remove("is-active");
+  });
+
+  //S12 Popup
+  const S12Button = document.getElementById("S12");
+  const S12modal = document.getElementById("S12md");
+  const S12modalBg = document.getElementById("S12bg");
+  const S12modalClose = document.getElementById("S12close");
+
+  S12Button.addEventListener('click', () => 
+  {
+    S12modal.classList.add("is-active");
+  });
+
+    S12modalBg.addEventListener('click', () => 
+  {
+    S12modal.classList.remove("is-active");
+  });
+
+    S12modalClose.addEventListener('click', () => 
+  {
+    S12modal.classList.remove("is-active");
+  });
+
+  //S13 Popup
+  const S13Button = document.getElementById("S13");
+  const S13modal = document.getElementById("S13md");
+  const S13modalBg = document.getElementById("S13bg");
+  const S13modalClose = document.getElementById("S13close");
+
+  S13Button.addEventListener('click', () => 
+  {
+    S13modal.classList.add("is-active");
+  });
+
+    S13modalBg.addEventListener('click', () => 
+  {
+    S13modal.classList.remove("is-active");
+  });
+
+    S13modalClose.addEventListener('click', () => 
+  {
+    S13modal.classList.remove("is-active");
+  });
+
+
 
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
